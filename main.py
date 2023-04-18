962100e74b1e065fd03210db773dadc49dcb223b
itdice <itdiceofficial@gmail.com>
2023-04-19 02:35:48 +0900

    eventlet 기반 Test Server 구축

--- main.py
+++ main.py
@@ -4,32 +4,53 @@
 #
 # Created by IT DICE on 2023/04/17.
 #
-import uvicorn
+import eventlet
 import socketio
 
-sio = socketio.AsyncServer()
-app = socketio.ASGIApp(sio)
+sio = socketio.Server(async_mode='eventlet')
+app = socketio.WSGIApp(sio)
 
 
 @sio.event
-async def connect(sid, environ, auth):
-    print(f'connected auth = {auth} sid = {sid}')
-    await sio.emit('hello', {'message': 'Hello',
-                             "hex": 0xAABBCCDDEEFF,
-                             "int": 123456789,
-                             "double": 1.23456789
-                             }, to=sid)
+def connect(sid, environ):
+    print("--------------------------------------------")
+    print(f"Client [{sid}] Connected!!!")
+    print("--------------------------------------------")
 
 
 @sio.event
 def disconnect(sid):
-    print(f"disconnected sid = {sid}")
+    print("--------------------------------------------")
+    print(f"Client [{sid}] Disconnected!!!")
+    print("--------------------------------------------")
 
 
-@sio.on("test")
-def test_event(sid, data):
-    print(f"Test sid = {sid} data = {data}")
+@sio.on("test_send")
+def test_send(sid, data):
+    print("--------------------------------------------")
+    print(f"Client [{sid}] send Data")
+    print(f"Data = {data}")
+    print("--------------------------------------------")
+
+
+@sio.on("test_request")
+def test_request(sid):
+    print("--------------------------------------------")
+    print(f"Client [{sid}] request Test Data!!!")
+    print("--------------------------------------------")
+
+    floatdata = 1.23456789
+
+    sio.emit('test_response', {'message': 'Test connection from Server',
+                               "hex": 0xAABBCCDDEEFF,
+                               "int": 123456789,
+                               "float": floatdata
+                               }, room=sid)
+
+    print("--------------------------------------------")
+    print(f"Test Data Send to Client [{sid}]")
+    print("--------------------------------------------")
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8710)
+    eventlet.wsgi.server(eventlet.listen(('', 8710)), app, log_output=False)
