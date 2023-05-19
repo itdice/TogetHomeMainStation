444ea2ddb51561db83ad690eef065bf404ddb627
itdice <itdiceofficial@gmail.com>
2023-05-20 05:42:26 +0900

    통합실행 파일 작성

--- toget_home_main_station.py
+++ toget_home_main_station.py
@@ -5,3 +5,57 @@
 # Created by IT DICE on 2023/04/30.
 #
 import subprocess
+import threading
+
+
+def read_subprocess_output(_process: subprocess.Popen):
+    while True:
+        output = _process.stdout.readline()
+        if output == b'' and _process.poll() is not None:
+            break
+        if output:
+            print(output.decode().strip())
+
+
+if __name__ == '__main__':
+    print("--------------------------------------------")
+    print("Start Server!!!")
+    print("--------------------------------------------")
+
+    # Subprocess Part
+    subprocess_list = [
+        subprocess.Popen(["python", "Connection/device_connect_manager.py"],
+                         stdout=subprocess.PIPE,
+                         universal_newlines=True),
+        subprocess.Popen(["python", "Connection/external_connect_manager.py"],
+                         stdout=subprocess.PIPE,
+                         universal_newlines=True),
+        subprocess.Popen(["python", "Database/json_server.py"],
+                         stdout=subprocess.PIPE,
+                         universal_newlines=True)
+    ]
+
+    # Thread Part
+    thread_list = []
+    for process in subprocess_list:
+        thread = threading.Thread(target=read_subprocess_output, args=(process,))
+        thread.daemon = True
+        thread.start()
+        thread_list.append(thread)
+
+    try:
+        while True:
+            pass  # infinite loop
+    except KeyboardInterrupt:
+        print("--------------------------------------------")
+        print("Stopping Server!!!")
+        print("--------------------------------------------")
+
+        for process in subprocess_list:
+            process.terminate()
+        for thread in thread_list:
+            thread.join()
+
+        print("--------------------------------------------")
+        print("Stopped")
+        print("--------------------------------------------")
